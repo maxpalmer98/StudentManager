5e5ae039ba5b8d8ccf12c7a88857c20a08bcbbea
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-04-30 18:04:48 -0700

    Creates a correct header for the student class

--- student.h
+++ student.h
@@ -1,14 +1,15 @@
+#include <string>
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
 class student
 {
 private:
-string firstName;
-string lastName;
+std::string first;
+std::string last;
 public:
-void setName(string, string);
-string fullName();
+void setName(std::string &, std::string &);
+std::string fullName();
 };
 
 #endif
